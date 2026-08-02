ffb7ecb8d33a70c57b3ed0ff02b9214017483302
chrisjnrod-hue <chrisjnrod@gmail.com>
2026-08-02 20:28:40 +0100

    Rename 24h_volumes to _24h_volumes

--- src/scanner_service.py
+++ src/scanner_service.py
@@ -103,7 +103,7 @@ class Scanner:
         self.task: Optional[asyncio.Task] = None
         self.rest_poller_task: Optional[asyncio.Task] = None
         self.callbacks: List[Callable[[str, Any], Any]] = []
-        self.24h_volumes: Dict[str, Dict[str, float]] = {}
+        self._24h_volumes: Dict[str, Dict[str, float]] = {}
         self.last_price_cache: Dict[str, float] = {}
         self.last_price_time: Dict[str, float] = {}
         self.mtf_monitoring: Dict[str, Dict[str, Any]] = {}
